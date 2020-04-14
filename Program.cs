f9a70d3b0b9bc153e5954e7f7891ca796073d1c4
Andrej Kyselica <andrej@kyselica.com>
2020-04-14 13:12:16 -0400

    initial version; listing metrics

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+// Azure Management dependencies
+using Microsoft.Rest.Azure.Authentication;
+using Microsoft.Rest.Azure.OData;
+
+// These examples correspond to the Monitor .Net SDK versions 0.16.0-preview and 0.16.1-preview
+// Those versions include the single-dimensional metrics API.
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
+using Microsoft.Azure.Management.Monitor;
+using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.Monitor.Fluent;
+using Microsoft.Azure.Management.Monitor.Fluent.Models;
+
+namespace monitor_metrics_bridge
+{
+  public class Program
+  {
+    // private static MonitorClient readOnlyClient;
+
+    public static void Main(string[] args)
+    {
+      DotNetEnv.Env.Load();
+
+      // supported metrics list: https://docs.microsoft.com/en-us/azure/azure-monitor/platform/metrics-supported
+
+      // Read CLI args
+      if (args.Length < 1)
+      {
+        throw new ArgumentException($"Usage: AzureMonitorCSharpExamples <resourceId> <namespace> (num supplied: {args.Length})");
+      }
+      string resourceId = args[0];
+
+      // Create Azure ARM connection, auth
+      Console.WriteLine("Reading config from env variables");
+      var tenantId = Environment.GetEnvironmentVariable("AZURE_TENANT_ID");
+      var clientId = Environment.GetEnvironmentVariable("AZURE_CLIENT_ID");
+      var secret = Environment.GetEnvironmentVariable("AZURE_CLIENT_SECRET");
+      var subscriptionId = Environment.GetEnvironmentVariable("AZURE_SUBSCRIPTION_ID");
+
+      Console.WriteLine($"Authenticating to Azure");
+
+      AzureCredentials credentials = SdkContext.AzureCredentialsFactory.FromServicePrincipal(clientId, secret, tenantId, AzureEnvironment.AzureGlobalCloud);
+      IAzure azure = Azure
+              .Authenticate(credentials)
+              .WithDefaultSubscription();
+
+
+      // Get a list of available metrics, types
+      Console.WriteLine($"Reading metrics for resource ${resourceId}");
+
+      var metricDefs = azure.MetricDefinitions.ListByResource(resourceId);
+
+      foreach (var md in metricDefs)
+      {
+        Console.WriteLine($"{md.Name.Value} : {md.Unit}");
+      }
+
+    }
+
+  }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# Azure Monitor Metrics Extract Sample
+
+This sample demonstrates extracting Azure Monitor metrics data using the Azure RM Fluent C# SDK with .NET Core.
+
+## Usage
+
+Create a .env file and/or set the following environemnt variables:
+
+```
+AZURE_TENANT_ID={guid}
+AZURE_CLIENT_ID={guid}
+AZURE_CLIENT_SECRET={guid}
+AZURE_SUBSCRIPTION_ID={guid}
+```
+
+Run the component with a single CLI argument containg the Resource ID on the metrics (format looks something like
+`/subscriptions/0000000-0000-0000-0000-000000000/resourceGroups/rgname/providers/Microsoft.Service/xxx`) 
+
+``` bash
+dotnet run --- /subscriptions/0000000-0000-0000-0000-000000000/resourceGroups/rgname/providers/Microsoft.Service/xxx
+```
